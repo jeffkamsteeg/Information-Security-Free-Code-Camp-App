a6ac6e7b60f486810b9e1193674aa03b7c59950a
Jeffry Kamsteeg Rodiño <j.kamsteeg1570@pascualbravo.edu.co>
2023-07-18 21:47:58 -0500

    Add initial application

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
-
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, () => {
+  console.log(`Useful Programmer Info Security App started on port ${PORT}`);
+});
 
 
 
